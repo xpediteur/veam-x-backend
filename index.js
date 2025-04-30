b80b1e48c03902d4e3194a8d790f934c4992c81c
Ralf <ralf.borde@hotmail.de>
2025-04-30 12:37:18 +0200

    2te

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Client, Users, Databases, Query } from 'node-appwrite';
 import moment from 'moment-timezone';
 
-// This Appwrite function will be executed every time your function is triggered new backend VEAM
+// This Appwrite function will be executed every time your function is triggered new backend VEAM-X
 export default async ({ req, res, log, error }) => {
     // You can use the Appwrite SDK to interact with other services
     // For this example, we're using the Users service
