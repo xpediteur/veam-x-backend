d135f0aae21715739399b79cc5b5680d0893cf48
Ralf <ralf.borde@hotmail.de>
2025-04-30 13:58:58 +0200

    neu

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import moment from 'moment-timezone';
 export default async ({ req, res, log, error }) => {
     // You can use the Appwrite SDK to interact with other services
     // For this example, we're using the Users service
-    const client = new Client()
-        .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-        .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-        .setKey(req.headers['x-appwrite-key'] ?? '');
+    /*     const client = new Client()
+            .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+            .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+            .setKey(req.headers['x-appwrite-key'] ?? ''); */
 
     /*     const users = new Users(client);
     
